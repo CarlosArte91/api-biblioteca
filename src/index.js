9dfd36ec0ec4030d43f4bc69a549e3fff088100b
Carlos Cruz <carturo99@gmail.com>
2023-06-10 21:40:01 -0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes/index.routes.js'
+import { PORT } from './config.js'
 
 const app = express()
 
@@ -8,5 +9,5 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', routes)
 
-app.listen(3000)
-console.log('Server running on port 3000')
+app.listen(PORT)
+console.log('Server running on port', PORT)
